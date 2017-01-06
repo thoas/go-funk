3616948e1973ca51bf06b5fdfbade64c3765ec53
Florent Messa <florent.messa@gmail.com>
2017-01-06 14:33:48 +0100

    Add funk.Values and funk.Keys

--- funk_test.go
+++ funk_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -355,4 +356,32 @@ func TestGetNil(t *testing.T) {
 
 	assert.Equal(Get(foo2, "Bar.Name"), nil)
 	assert.Equal(Get([]*Foo{foo, foo2}, "Bar.Name"), []string{"Test"})
+}
+
+func TestKeys(t *testing.T) {
+	assert := assert.New(t)
+
+	results := Keys(map[string]int{"one": 1, "two": 2}).([]string)
+	sort.Strings(results)
+
+	assert.Equal(results, []string{"one", "two"})
+
+	fields := Keys(foo).([]string)
+
+	sort.Strings(fields)
+
+	assert.Equal(fields, []string{"Age", "Bar", "Bars", "EmptyValue", "FirstName", "ID", "LastName"})
+}
+
+func TestValues(t *testing.T) {
+	assert := assert.New(t)
+
+	results := Values(map[string]int{"one": 1, "two": 2}).([]int)
+	sort.Ints(results)
+
+	assert.Equal(results, []int{1, 2})
+
+	values := Values(foo).([]interface{})
+
+	assert.Len(values, 7)
 }
