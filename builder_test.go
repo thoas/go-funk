e7860aeabaa46da33fe9593b4983045493be174b
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2018-08-16 20:24:43 +0200

    add builder examples

--- builder_test.go
+++ builder_test.go
@@ -132,3 +132,71 @@ func TestLazyChainWith(t *testing.T) {
 		})
 	}
 }
+
+func ExampleChain() {
+	v := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	chain := Chain(v)
+	lazy := LazyChain(v)
+
+	// Without builder
+	a := Filter(v, func(x int) bool { return x%2 == 0 })
+	b := Map(a, func(x int) int { return x * 2 })
+	c := Reverse(a)
+	fmt.Printf("funk.Contains(b, 2): %v\n", Contains(b, 2)) // false
+	fmt.Printf("funk.Contains(b, 4): %v\n", Contains(b, 4)) // true
+	fmt.Printf("funk.Sum(b): %v\n", Sum(b))                 // 40
+	fmt.Printf("funk.Head(b): %v\n", Head(b))               // 4
+	fmt.Printf("funk.Head(c): %v\n\n", Head(c))             // 8
+
+	// With simple chain builder
+	ca := chain.Filter(func(x int) bool { return x%2 == 0 })
+	cb := ca.Map(func(x int) int { return x * 2 })
+	cc := ca.Reverse()
+	fmt.Printf("chainB.Contains(2): %v\n", cb.Contains(2)) // false
+	fmt.Printf("chainB.Contains(4): %v\n", cb.Contains(4)) // true
+	fmt.Printf("chainB.Sum(): %v\n", cb.Sum())             // 40
+	fmt.Printf("chainB.Head(): %v\n", cb.Head())           // 4
+	fmt.Printf("chainC.Head(): %v\n\n", cc.Head())         // 8
+
+	// With lazy chain builder
+	la := lazy.Filter(func(x int) bool { return x%2 == 0 })
+	lb := la.Map(func(x int) int { return x * 2 })
+	lc := la.Reverse()
+	fmt.Printf("lazyChainB.Contains(2): %v\n", lb.Contains(2)) // false
+	fmt.Printf("lazyChainB.Contains(4): %v\n", lb.Contains(4)) // true
+	fmt.Printf("lazyChainB.Sum(): %v\n", lb.Sum())             // 40
+	fmt.Printf("lazyChainB.Head(): %v\n", lb.Head())           // 4
+	fmt.Printf("lazyChainC.Head(): %v\n", lc.Head())           // 8
+}
+
+type updatingStruct struct {
+	x []int
+}
+
+func (us *updatingStruct) Values() interface{} {
+	return us.x
+}
+
+func ExampleLazyChainUtility() {
+	us := updatingStruct{}
+	chain := Chain(us.x).
+		Map(func(x int) float64 { return float64(x) * 2.5 })
+	lazy := LazyChain(us.x).
+		Map(func(x int) float64 { return float64(x) * 2.5 })
+	lazyWith := LazyChainWith(us.Values).
+		Map(func(x int) float64 { return float64(x) * 2.5 })
+
+	fmt.Printf("chain.Sum(): %v\n", chain.Sum())         // 0
+	fmt.Printf("lazy.Sum(): %v\n", lazy.Sum())           // 0
+	fmt.Printf("lazyWith.Sum(): %v\n\n", lazyWith.Sum()) // 0
+
+	us.x = append(us.x, 2)
+	fmt.Printf("chain.Sum(): %v\n", chain.Sum())         // 0
+	fmt.Printf("lazy.Sum(): %v\n", lazy.Sum())           // 0
+	fmt.Printf("lazyWith.Sum(): %v\n\n", lazyWith.Sum()) // 5
+
+	us.x = append(us.x, 10)
+	fmt.Printf("chain.Sum(): %v\n", chain.Sum())         // 0
+	fmt.Printf("lazy.Sum(): %v\n", lazy.Sum())           // 0
+	fmt.Printf("lazyWith.Sum(): %v\n\n", lazyWith.Sum()) // 30
+}
