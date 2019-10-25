a39c1e572ff844e00314bae20fe4cc067f65cd92
Alena Egorova <catincosmicspace@gmail.com>
2019-10-25 11:22:32 +0300

    Add Min method

--- min.go
+++ min.go
@@ -2,193 +2,192 @@ package funk
 
 import "strings"
 
-// MaxInt validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinInt validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []int
 // It returns int or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxInt(i []int) interface{} {
+func MinInt(i []int) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max int
+	var min int
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxInt8 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinInt8 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []int8
 // It returns int8 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxInt8(i []int8) interface{} {
+func MinInt8(i []int8) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max int8
+	var min int8
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxInt16 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinInt16 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []int16
 // It returns int16 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxInt16(i []int16) interface{} {
+func MinInt16(i []int16) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max int16
+	var min int16
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxInt32 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinInt32 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []int32
 // It returns int32 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxInt32(i []int32) interface{} {
+func MinInt32(i []int32) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max int32
+	var min int32
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxInt64 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinInt64 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []int64
 // It returns int64 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxInt64(i []int64) interface{} {
+func MinInt64(i []int64) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max int64
+	var min int64
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxFloat32 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinFloat32 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []float32
 // It returns float32 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxFloat32(i []float32) interface{} {
+func MinFloat32(i []float32) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max float32
+	var min float32
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxFloat64 validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinFloat64 validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []float64
 // It returns float64 or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxFloat64(i []float64) interface{} {
+func MinFloat64(i []float64) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max float64
+	var min float64
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		if item > max {
-			max = item
+		if item < min {
+			min = item
 		}
 	}
-	return max
+	return min
 }
 
-// MaxString validates the input, compares the elements and returns the maximum element in an array/slice.
+// MinString validates the input, compares the elements and returns the minimum element in an array/slice.
 // It accepts []string
 // It returns string or nil
 // It returns nil for the following cases:
 //  - input is of length 0
-func MaxString(i []string) interface{} {
+func MinString(i []string) interface{} {
 	if len(i) == 0 {
 		return nil
 	}
-	var max string
+	var min string
 	for idx := 0; idx < len(i); idx++ {
 		item := i[idx]
 		if idx == 0 {
-			max = item
+			min = item
 			continue
 		}
-		max = compareStringsMax(max, item)
+		min = compareStringsMin(min, item)
 	}
-	return max
+	return min
 }
 
-// compareStrings uses the strings.Compare method to compare two strings, and returns the greater one.
-func compareStringsMax(max, current string) string {
-	r := strings.Compare(strings.ToLower(max), strings.ToLower(current))
-	if r > 0 {
-		return max
+func compareStringsMin(min, current string) string {
+	r := strings.Compare(strings.ToLower(min), strings.ToLower(current))
+	if r < 0 {
+		return min
 	}
 	return current
 }
